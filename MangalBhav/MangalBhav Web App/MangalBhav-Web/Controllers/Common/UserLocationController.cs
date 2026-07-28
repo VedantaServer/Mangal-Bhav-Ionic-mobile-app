8ba923ec491a312b277d9ba79199aa41849dc5b6
VedantaServer <vedantaserver01@gmail.com>
2026-07-28 14:26:45 +0530

    checklocation committ

---
+++ MangalBhav/MangalBhav Web App/MangalBhav-Web/Controllers/Common/UserLocationController.cs
@@ -0,0 +1,58 @@
+﻿using FaceUPAI.DataAccessService;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace FaceUPAI.Controllers.Common
+{
+    public class UserLocationController : Controller
+    {
+        /// <summary>
+        /// Checks for nearby mandirs for a given user location and queues
+        /// notifications for any matches (deduped over the last 24 hours).
+        /// </summary>
+        [HttpPost]
+        [EnableCors("AllowAll")]
+        public IActionResult CheckNearbyMandir([FromBody] NearbyMandirRequest request)
+        {
+            if (request == null)
+                return BadRequest("Request body is required.");
+
+            if (request.UserId <= 0)
+                return BadRequest("A valid UserId is required.");
+
+            if (request.Lat < -90 || request.Lat > 90 || request.Lng < -180 || request.Lng > 180)
+                return BadRequest("Lat/Lng are out of valid range.");
+
+
+            return ApiHandler.Handle(() =>
+            {
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                new SqlParameter("@UserId", request.UserId == 0 ? SqlInt32.Null : request.UserId),
+                new SqlParameter("@Lat", request.Lat),
+                new SqlParameter("@Lng", request.Lng),
+                new SqlParameter("@RadiusKm", request.RadiusKm == null ? SqlDouble.Null : request.RadiusKm)
+                };
+                DataAccess.ExecuteNonQuery(CommandType.StoredProcedure, "sp_CheckNearbyMandirAndNotify", parameters);
+                return Ok(new { message = "Nearby mandir check completed." });
+            }, this);
+        }
+
+        // Add sibling actions here as more entities come online, e.g.:
+        // [HttpPost]
+        // public IActionResult CheckNearbyPandit([FromBody] NearbyPanditRequest request) { ... }
+        // [HttpPost]
+        // public IActionResult CheckNearbyEvent([FromBody] NearbyEventRequest request) { ... }
+    }
+
+    public class NearbyMandirRequest
+    {
+        public int UserId { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public double? RadiusKm { get; set; }
+    }
+}
--- MangalBhav/MangalBhav Web App/MangalBhav-Web/Controllers/MasterDataAPI.cs
+++ MangalBhav/MangalBhav Web App/MangalBhav-Web/Controllers/MasterDataAPI.cs
@@ -1,166 +1,164 @@
-using System;
-using Microsoft.AspNetCore.Mvc;
-using System.Data;
-using Microsoft.Data.SqlClient;
-using System.Data.SqlTypes;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization;
-using FaceUPAI.Models;
 using FaceUPAI.DataAccessService;
-using FaceUPAI.Controllers.Common;
+using FaceUPAI.Models;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlTypes;
 
 namespace FaceUPAI.API
 {
-	public class MasterDataAPI : ControllerBase
-	{
-	public IActionResult Index()
-	{
-	return View();
-	}
-	private IActionResult View()
-	{
-	throw new NotImplementedException();
-	}
-		/// <summary>
-		/// Saves a record to the MasterData table.
-		/// </summary>
-	[HttpPost]
-		[EnableCors("AllowAll")]
-		[Route("MasterDataInsert")]
-		public  IActionResult MasterDataInsert([FromBody] MasterData masterData)
-		{
-			return ApiHandler.Handle(() =>
-	{
+    public class MasterDataAPI : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Saves a record to the MasterData table.
+        /// </summary>
+        [HttpPost]
+        [EnableCors("AllowAll")]
+        [Route("MasterDataInsert")]
+        public IActionResult MasterDataInsert([FromBody] MasterData masterData)
+        {
+            return ApiHandler.Handle(() =>
+    {
 
-			SqlParameter[] parameters = new SqlParameter[]
-			{
-				new SqlParameter("@TenantID", masterData.TenantID == 0 ? SqlInt32.Null : masterData.TenantID ),
-				new SqlParameter("@ParentItemID", masterData.ParentItemID == 0 ? SqlInt32.Null : masterData.ParentItemID ),
-				new SqlParameter("@Description", masterData.Description),
-				new SqlParameter("@Domain", masterData.Domain),
-				new SqlParameter("@Identifier", masterData.Identifier),
-				new SqlParameter("@DateAdded", masterData.DateAdded == DateTime.MinValue ? SqlDateTime.Null : masterData.DateAdded ),
-				new SqlParameter("@DateModified", masterData.DateModified == DateTime.MinValue ? SqlDateTime.Null : masterData.DateModified ),
-				new SqlParameter("@UpdatedByUser", masterData.UpdatedByUser),
-				new SqlParameter("@MasterDataLevel", masterData.MasterDataLevel)
-			};
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+                new SqlParameter("@TenantID", masterData.TenantID == 0 ? SqlInt32.Null : masterData.TenantID ),
+                new SqlParameter("@ParentItemID", masterData.ParentItemID == 0 ? SqlInt32.Null : masterData.ParentItemID ),
+                new SqlParameter("@Description", masterData.Description),
+                new SqlParameter("@Domain", masterData.Domain),
+                new SqlParameter("@Identifier", masterData.Identifier),
+                new SqlParameter("@DateAdded", masterData.DateAdded == DateTime.MinValue ? SqlDateTime.Null : masterData.DateAdded ),
+                new SqlParameter("@DateModified", masterData.DateModified == DateTime.MinValue ? SqlDateTime.Null : masterData.DateModified ),
+                new SqlParameter("@UpdatedByUser", masterData.UpdatedByUser),
+                new SqlParameter("@MasterDataLevel", masterData.MasterDataLevel)
+        };
 
-			masterData.MasterDataID = Convert.ToInt32(DataAccess.ExecuteScalar(CommandType.StoredProcedure, "MasterDataInsert", parameters));
-			return Ok(new {MasterDataID=masterData.MasterDataID});
-			}, this);
-		}
-
-
-		[HttpPost]
-		[EnableCors("AllowAll")]
-		[Route("MasterDataUpdate")]
-		public IActionResult MasterDataUpdate([FromBody] MasterData masterData)
-		{
-			return ApiHandler.Handle(() =>
-			{
-
-				SqlParameter[] parameters = new SqlParameter[]
-				{
-				new SqlParameter("@MasterDataID", masterData.MasterDataID == 0 ? SqlInt32.Null : masterData.MasterDataID ),
-				new SqlParameter("@TenantID", masterData.TenantID == 0 ? SqlInt32.Null : masterData.TenantID ),
-				new SqlParameter("@ParentItemID", masterData.ParentItemID == 0 ? SqlInt32.Null : masterData.ParentItemID ),
-				new SqlParameter("@Description", masterData.Description),
-				new SqlParameter("@Domain", masterData.Domain),
-				new SqlParameter("@Identifier", masterData.Identifier),
-				new SqlParameter("@DateAdded", masterData.DateAdded == DateTime.MinValue ? SqlDateTime.Null : masterData.DateAdded ),
-				new SqlParameter("@DateModified", masterData.DateModified == DateTime.MinValue ? SqlDateTime.Null : masterData.DateModified ),
-				new SqlParameter("@UpdatedByUser", masterData.UpdatedByUser),
-				new SqlParameter("@MasterDataLevel", masterData.MasterDataLevel)
-				};
-
-				var MasterDataID = Convert.ToInt32(DataAccess.ExecuteNonQuery(CommandType.StoredProcedure, "MasterDataUpdate", parameters));
-				return Ok(new { MasterDataID = MasterDataID });
-			}, this);
-		}
+        masterData.MasterDataID = Convert.ToInt32(DataAccess.ExecuteScalar(CommandType.StoredProcedure, "MasterDataInsert", parameters));
+        return Ok(new { MasterDataID = masterData.MasterDataID });
+    }, this);
+        }
 
 
+        [HttpPost]
+        [EnableCors("AllowAll")]
+        [Route("MasterDataUpdate")]
+        public IActionResult MasterDataUpdate([FromBody] MasterData masterData)
+        {
+            return ApiHandler.Handle(() =>
+            {
 
-		/// <summary>
-		/// Selects all records from the MasterData table by a foreign key.
-		/// </summary>
-		[HttpPost]
-		[EnableCors("AllowAll")]
-		[Route("MasterDataSelectAllByTenantID")]
-		public  IActionResult MasterDataSelectAllByTenantID(int tenantID)
-	{
-			return ApiHandler.Handle(() =>
-	{
-			SqlParameter[] parameters = new SqlParameter[]
-			{
-				new SqlParameter("@TenantID", tenantID)
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                new SqlParameter("@MasterDataID", masterData.MasterDataID == 0 ? SqlInt32.Null : masterData.MasterDataID ),
+                new SqlParameter("@TenantID", masterData.TenantID == 0 ? SqlInt32.Null : masterData.TenantID ),
+                new SqlParameter("@ParentItemID", masterData.ParentItemID == 0 ? SqlInt32.Null : masterData.ParentItemID ),
+                new SqlParameter("@Description", masterData.Description),
+                new SqlParameter("@Domain", masterData.Domain),
+                new SqlParameter("@Identifier", masterData.Identifier),
+                new SqlParameter("@DateAdded", masterData.DateAdded == DateTime.MinValue ? SqlDateTime.Null : masterData.DateAdded ),
+                new SqlParameter("@DateModified", masterData.DateModified == DateTime.MinValue ? SqlDateTime.Null : masterData.DateModified ),
+                new SqlParameter("@UpdatedByUser", masterData.UpdatedByUser),
+                new SqlParameter("@MasterDataLevel", masterData.MasterDataLevel)
+                };
 
-			};
+                var MasterDataID = Convert.ToInt32(DataAccess.ExecuteNonQuery(CommandType.StoredProcedure, "MasterDataUpdate", parameters));
+                return Ok(new { MasterDataID = MasterDataID });
+            }, this);
+        }
 
-			using (SqlDataReader dataReader = DataAccess.ExecuteReader( System.Data.CommandType.StoredProcedure, "MasterDataSelectAllByTenantID", parameters))
-			{
-				List<MasterData> MasterDataList = new List<MasterData>();
-				while (dataReader.Read())
-				{
-					MasterData MasterData = MakeMasterData(dataReader);
-					MasterDataList.Add(MasterData);
-				}
 
-				return  Ok(new {MasterDataList});
-			}
-			}, this);
-		}
 
-		/// <summary>
-		/// Selects all query records from the MasterData table by a ak=ll query.
-		/// </summary>
-	[HttpPost]
-		[EnableCors("AllowAll")]
-		[Route("MasterDataSelectByQuery")]
-		public  IActionResult MasterDataSelectByQuery(int tenantID,string Query)
-		
-		{
-			return ApiHandler.Handle(() =>
-	{
-			SqlParameter[] parameters = new SqlParameter[]
-			{
-				new SqlParameter("@TenantID", tenantID),							new SqlParameter("@Query", Query),
+        /// <summary>
+        /// Selects all records from the MasterData table by a foreign key.
+        /// </summary>
+        [HttpPost]
+        [EnableCors("AllowAll")]
+        [Route("MasterDataSelectAllByTenantID")]
+        public IActionResult MasterDataSelectAllByTenantID(int tenantID)
+        {
+            return ApiHandler.Handle(() =>
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+                new SqlParameter("@TenantID", tenantID)
 
-			};
+        };
 
-			using (SqlDataReader dataReader = DataAccess.ExecuteReader( System.Data.CommandType.StoredProcedure, "MasterDataSelectByQuery", parameters))
-			{
-				List<MasterData> MasterDataList = new List<MasterData>();
-				while (dataReader.Read())
-				{
-					MasterData MasterData = MakeMasterData(dataReader);
-					MasterDataList.Add(MasterData);
-				}
+        using (SqlDataReader dataReader = DataAccess.ExecuteReader(System.Data.CommandType.StoredProcedure, "MasterDataSelectAllByTenantID", parameters))
+        {
+            List<MasterData> MasterDataList = new List<MasterData>();
+            while (dataReader.Read())
+            {
+                MasterData MasterData = MakeMasterData(dataReader);
+                MasterDataList.Add(MasterData);
+            }
 
-				return  Ok(new {MasterDataList});
-			}
-			}, this);
-		}
+            return Ok(new { MasterDataList });
+        }
+    }, this);
+        }
 
-		/// <summary>
-		/// Creates a new instance of the MasterData class and populates it with data from the specified SqlDataReader.
-		/// </summary>
-		public  MasterData MakeMasterData(SqlDataReader dataReader)
-		{
-			MasterData masterData = new MasterData();
-			masterData.MasterDataID = DataAccess.GetInt32(dataReader, "MasterDataID", 0);
-			masterData.TenantID = DataAccess.GetInt32(dataReader, "TenantID", 0);
-			masterData.ParentItemID = DataAccess.GetInt32(dataReader, "ParentItemID", 0);
-			masterData.Description = DataAccess.GetString(dataReader, "Description", String.Empty);
-			masterData.Domain = DataAccess.GetString(dataReader, "Domain", String.Empty);
-			masterData.Identifier = DataAccess.GetString(dataReader, "Identifier", String.Empty);
-			masterData.DateAdded = DataAccess.GetDateTime(dataReader, "DateAdded", DateTime.MinValue);
-			masterData.DateModified = DataAccess.GetDateTime(dataReader, "DateModified", DateTime.MinValue);
-			masterData.UpdatedByUser = DataAccess.GetString(dataReader, "UpdatedByUser", String.Empty);
-			masterData.MasterDataLevel = DataAccess.GetString(dataReader, "MasterDataLevel", String.Empty);
+        /// <summary>
+        /// Selects all query records from the MasterData table by a ak=ll query.
+        /// </summary>
+        [HttpPost]
+        [EnableCors("AllowAll")]
+        [Route("MasterDataSelectByQuery")]
+        public IActionResult MasterDataSelectByQuery(int tenantID, string Query)
 
-			return masterData;
-		}
+        {
+            return ApiHandler.Handle(() =>
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+                new SqlParameter("@TenantID", tenantID),                            new SqlParameter("@Query", Query),
 
-	}
-	}
+        };
+
+        using (SqlDataReader dataReader = DataAccess.ExecuteReader(System.Data.CommandType.StoredProcedure, "MasterDataSelectByQuery", parameters))
+        {
+            List<MasterData> MasterDataList = new List<MasterData>();
+            while (dataReader.Read())
+            {
+                MasterData MasterData = MakeMasterData(dataReader);
+                MasterDataList.Add(MasterData);
+            }
+
+            return Ok(new { MasterDataList });
+        }
+    }, this);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the MasterData class and populates it with data from the specified SqlDataReader.
+        /// </summary>
+        public MasterData MakeMasterData(SqlDataReader dataReader)
+        {
+            MasterData masterData = new MasterData();
+            masterData.MasterDataID = DataAccess.GetInt32(dataReader, "MasterDataID", 0);
+            masterData.TenantID = DataAccess.GetInt32(dataReader, "TenantID", 0);
+            masterData.ParentItemID = DataAccess.GetInt32(dataReader, "ParentItemID", 0);
+            masterData.Description = DataAccess.GetString(dataReader, "Description", String.Empty);
+            masterData.Domain = DataAccess.GetString(dataReader, "Domain", String.Empty);
+            masterData.Identifier = DataAccess.GetString(dataReader, "Identifier", String.Empty);
+            masterData.DateAdded = DataAccess.GetDateTime(dataReader, "DateAdded", DateTime.MinValue);
+            masterData.DateModified = DataAccess.GetDateTime(dataReader, "DateModified", DateTime.MinValue);
+            masterData.UpdatedByUser = DataAccess.GetString(dataReader, "UpdatedByUser", String.Empty);
+            masterData.MasterDataLevel = DataAccess.GetString(dataReader, "MasterDataLevel", String.Empty);
+
+            return masterData;
+        }
+
+    }
+}
